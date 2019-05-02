d29843a23613861ed9cc2fd552e41bef349061fc
Erin Payne <enpayne@ksu.edu>
2019-05-02 01:11:50 -0500

    Integrating first few MPI functions

--- MPI/mpi.c
+++ MPI/mpi.c
@@ -1,53 +1,76 @@
-#include <omp.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define NUM_THREADS 4
-#define WIKI_ARRAY_SIZE 8
-#define MAX_ENTRY_LENGTH 100
+#define WIKI_ARRAY_SIZE 50
+#define MAX_ENTRY_LENGTH 1000
 
 char wiki_array[WIKI_ARRAY_SIZE][MAX_ENTRY_LENGTH];
 char substrings[WIKI_ARRAY_SIZE-1][MAX_ENTRY_LENGTH];
+int lines_read;
+int batch_number = 0;
+int num_threads;
 
-void readFile();
+int readFile(FILE *);
 void calcSubstring(int);
 void printResults();
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
-int main() {
-	omp_set_num_threads(NUM_THREADS);
-	
-	readFile();
+int main(int argc, char *argv[]) {
+	int num_tasks, rank, ierr;
 
-	#pragma omp parallel 
+	MPI_Status status;
+	MPI_Request request;
+
+	ierr = MPI_Init(&argc, &argv);
+	FILE * fp = fopen("/homes/dan/625/wiki_dump.txt","r");
+
+	if (ie != MPI_SUCCESS)
 	{
-		calcSubstring(omp_get_thread_num());
+		printf("Error");
+		MPI_Abort(MPI_COMM_WORLD, id);
 	}
-	
-	printResults();
+	else {
+		MPI_Comm_Size(MPI_COMM_WORLD, &num_tasks);
+		MPI_Comm_Rank(MPI_COMM_WORLD, &rank);
+		num_threads = num_tasks;
+
+		lines_read = readFile(fp);
+
+		if (lines_read == 0) break;
+
+		printResults();
+		batch_number++;
+		if (lines_read < WIKI_ARRAY_SIZE) break;
+	}
+	fclose(fp);
 	return 0;
 }
 
-void readFile()
+int readFile(FILE * fp)
 {
-	FILE * fp = fopen("test.txt","r");
 	if(fp == NULL)
 	{
 		printf("fail");
 		return;
 	}
-	
+
 	char buff[MAX_ENTRY_LENGTH];
 	int line_number = 0;
-	
-	while(fgets(buff,MAX_ENTRY_LENGTH,fp) != NULL)
+	int i;
+	for (i = 0; i < WIKI_ARRAY_SIZE; i++)
 	{
+		if (fgets(buff,MAX_ENTRY_LENGTH,fp) == NULL)
+		{
+			return i;
+		}
 		strcpy(wiki_array[line_number],buff);
 		line_number++;
 	}
-	fclose(fp);
+	return i;
 }
 
 void calcSubstring(int threadID)
@@ -59,18 +82,18 @@ void calcSubstring(int threadID)
 	int m; //length of string 1
 	int n; //length of string 2
 	int index;
-	
+
 	#pragma omp private(L, string1, string2, m, n, substring)
 	{
 		for (index = threadID; index < WIKI_ARRAY_SIZE - 1; index += NUM_THREADS)
 		{
 			string1 = wiki_array[index];
 			string2 = wiki_array[index+1];
-			m = strlen(string1); 
+			m = strlen(string1);
 			n = strlen(string2);
 			int L[m+1][n+1];
 			int i,j;
-			
+
 			//calculate the dynamic programming table
 			for (i = 0; i <=m; i++)
 			{
@@ -90,12 +113,12 @@ void calcSubstring(int threadID)
 					}
 				}
 			}
-			
+
 			//make another pass through the table to find the longest common substring
 			int s_index = 0;
 			i = 0;
 			j = 0;
-		
+
 			while (i < m && j < n)
 			{
 				if (string1[i] == string2[j])
@@ -107,7 +130,7 @@ void calcSubstring(int threadID)
 				else j++;
 			}
 			substring[s_index] = '\0';
-			
+
 			#pragma omp critical
 			{
 				strcpy(substrings[index],substring);
@@ -121,6 +144,6 @@ void printResults()
 	int i;
 	for (i = 0; i < WIKI_ARRAY_SIZE-1; i++)
 	{
-		printf("Line: %d, LCS: %s\n",i,substrings[i]);
+		printf("Line: %d, LCS: %s\n",batch_number*WIKI_ARRAY_SIZE + i,substrings[i]);
 	}
 }
