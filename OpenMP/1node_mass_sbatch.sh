e8f7612429d1cf0dc58776fca51ef7816d29b3fa
Laurel Thomson <laurel228@eos.beocat.ksu.edu>
2019-05-03 12:34:42 -0500

    adding longer time constraint to batch files

--- OpenMP/1node_mass_sbatch.sh
+++ OpenMP/1node_mass_sbatch.sh
@@ -2,5 +2,5 @@
 
 for i in 1 2 4 8 16
 do
-	 sbatch --constraint=elves --ntasks-per-node=${i} --nodes=1 openmp_sbatch.sh
+	 sbatch --constraint=elves --ntasks-per-node=${i} --nodes=1 --time=24:00:00 openmp_sbatch.sh
 done
--- Pthreads/mass_sbatch.sh
+++ Pthreads/mass_sbatch.sh
@@ -2,5 +2,5 @@
 
 for i in 1 2 4 8 16
 do
-	sbatch --constraint=elves --ntasks-per-node=${i} --nodes=1 sbatch.sh
+	sbatch --constraint=elves --ntasks-per-node=${i} --nodes=1 --time:24:00:00 sbatch.sh
 done
